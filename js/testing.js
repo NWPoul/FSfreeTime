81cc56e3f18483fe42f486f1c8d947bc65b83823
nwpoul <nwpoul@yandex.ru>
2019-06-06 18:58:09 +0300

    hEd

--- js/testing.js
+++ js/testing.js
@@ -647,18 +647,14 @@ function bench(testF, times) {
 
   var t = new Date();
   for (var i = 0; i < times; i++) {
-    // testingrunTable(GVAR.bookingArr, 'bookings');
+    testingrunTable(GVAR.bookingArr, 'bookings');
     // testingrunTable(GVAR.bookingObj, 'freetime');
-    proceedBookingData(GVAR.bookingData)
+    // proceedBookingData(GVAR.bookingData)
   }
   var result = (new Date() - t) / times;
   console.log(result);
   return result;
 }
-
-
-
-
 
 
 
@@ -697,6 +693,7 @@ if (GVAR.user.toLowerCase() == 'tst') {
     let dateStr = _date.msToCustomDateObj( Arr[ri][timeCol] );
 let freeTime = 30 - Arr[ri][timeValCol];
 let curTimeslotN = _date.msToSlotN( Arr[ri][timeCol] );
+let trId = (dateStr.dateN + '_' + dateStr.time);
 
     let trClassList = [];
 
@@ -716,7 +713,7 @@ let curTimeslotN = _date.msToSlotN( Arr[ri][timeCol] );
     }
     let trClassStr = trClassList.join(' ');
 
-    let trStrStart = '<tr ' +('class="' +trClassStr +'"') +'>';
+    let trStrStart = '<tr ' +('id="' +trId +'" ') +('class="' +trClassStr +'"') +'>';
     let trStrEnd = '</tr>';
 
     let trInnerHtmlStr = '<th ' + ('id="r' +ri +'c0" ') +'>' + // +('class="' +groupName +'"')
@@ -730,8 +727,8 @@ let curTimeslotN = _date.msToSlotN( Arr[ri][timeCol] );
             '</th>';
 
     for (let ci = 2; ci < colsN; ci++) {
-      let tdID = 'r' +ri +'c' +ci;
-      trInnerHtmlStr += '<td id="' +tdID +'">' +
+      //let tdID = 'r' +ri +'c' +ci;
+      trInnerHtmlStr += '<td>' +
                  Arr[ri][ci] +
                 '</td>';
     }// end for cols
@@ -744,3 +741,44 @@ let curTimeslotN = _date.msToSlotN( Arr[ri][timeCol] );
 }//=====END setBookingTable==================
 
 
+
+
+
+function scrollToCurrentTime() {
+  if (MODE != 'bookings') return;
+  let currentTime = Date.now();
+  let dateStr = _date.msToCustomDateObj(currentTime - GVAR.GMToffset); // - offset for UTC!!!
+  let targetTrId = (dateStr.dateN + '_' + dateStr.time);
+  console.log(targetTrId);
+  scrollToElement(targetTrId);
+  blinkElem(targetTrId, (dateStr.dayName + ', ' +dateStr.dayN + '/' +dateStr.monthN +'_' +dateStr.time) );
+}
+
+function scrollToElement(theElement) {
+  if (typeof theElement === 'string') {
+    theElement = document.getElementById(theElement);
+  }
+  theElement.scrollIntoView(
+    {
+      block: 'center',
+      behavior: 'smooth'
+    }
+  );
+}// end scrollToElement
+
+function blinkElem(elem, text='HERE!)') {
+  if (typeof(elem) === 'string') {
+    elem = document.getElementById(elem);
+  }
+
+  // let sourceVal = elem.innerHTML;
+  // let tempVal = '<span class="blinkElem">' +text + '</span>';
+  
+  elem.classList.toggle( 'blinkElem' );
+  
+  // setTimeout( (elem) => {
+  //   elem.classList.toggle( 'blinkElem' );
+  // }, 500);
+}
+
+ 
